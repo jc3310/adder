9f92a1e9d7d30b8933cb217331211df306399761
Sagar Parmar <108988109+Sagarparmar04@users.noreply.github.com>
2024-02-21 19:02:36 +0530

    Update halfAdder.v

--- halfAdder.v
+++ halfAdder.v
@@ -1,8 +1,8 @@
 module halfAdder (
-    input a, b,
+    input a1, b1,
     output sum, carry
 );   
    
-    assign sum = a ^ b;
-    assign carry = a & b;
+    assign sum = a1 ^ b1;
+    assign carry = a1 & b1;
 endmodule
